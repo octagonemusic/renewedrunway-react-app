d717130c1f969081cbe3cd6cab6401ca4d93985a
Dark Waves Tech <89140466+darkwaves-ofc@users.noreply.github.com>
2023-08-25 22:23:41 +0530

    Update shopping.jsx

--- src/components/shopping/shopping.jsx
+++ src/components/shopping/shopping.jsx
@@ -1,9 +1,23 @@
+import React from 'react';
+
 function Shopping() {
-    return(
-        <div>
-            <h1>Shopping</h1>
+  return (
+    <div className="container shopping">
+      <div className="left-nav">
+        <div className="selecter">
+          <div className="item">Mens</div>
+          <div className="item">Womens</div>
         </div>
-    )
+        <div className="nav-section">
+          <ul>
+            <li>
+                
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Shopping;
