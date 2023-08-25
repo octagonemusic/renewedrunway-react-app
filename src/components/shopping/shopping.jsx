c8bb3972118703f6258185334624175fcd241737
Bhargav Prasad <octagonemusic69@gmail.com>
2023-08-26 00:28:32 +0530

    Shopping Page Added

--- src/components/shopping/shopping.jsx
+++ src/components/shopping/shopping.jsx
@@ -1,19 +1,51 @@
-import React from 'react';
+import React from "react";
+import "./shopping.css"
 
 function Shopping() {
   return (
-    <div className="container shopping">
-      <div className="left-nav">
-        <div className="selecter">
-          <div className="item">Mens</div>
+    <div className="container shopping flex-row">
+      <div className="left-nav flex-col">
+        <div className="selector flex-row">
+          <div className="item active">Mens</div>
           <div className="item">Womens</div>
         </div>
         <div className="nav-section">
           <ul>
             <li>
-                
+              <img src="/shopping/star.svg" alt="" />
+              <span>New Arrivals</span>
+            </li>
+            <li>
+              <img src="/shopping/shirt.svg" alt="" />
+              <span>T-shirt</span>
+            </li>
+            <li className="active">
+              <img src="/shopping/trousers.svg" alt="" />
+              <span>Trousers</span>
+            </li>
+            <li>
+              <img src="/shopping/shoes.svg" alt="" />
+              <span>Shoes</span>
+            </li>
+            <li>
+              <img src="/shopping/accessories.svg" alt="" />
+              <span>Accessories</span>
+            </li>
+            <li>
+              <img src="/shopping/household.svg" alt="" />
+              <span>Household Accessories</span>
             </li>
           </ul>
+        </div>
+      </div>
+      <div className="content-container">
+        <div className="items">
+          <div className="item"></div>
+          <div className="item"></div>
+          <div className="item"></div>
+          <div className="item"></div>
+          <div className="item"></div>
+          <div className="item"></div>
         </div>
       </div>
     </div>
